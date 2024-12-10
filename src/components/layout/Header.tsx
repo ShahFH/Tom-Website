1429432ef1d72ecdc6d656072fe1d878e9bb5810
Shah Faisal <shahfaisal.swe@gmail.com>
2024-12-10 21:09:42 +0430

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -91,21 +91,21 @@ const Header = () => {
               <div className="absolute top-full left-0 right-0 sm:hidden bg-black/90 backdrop-blur-xl">
                 <div className="flex flex-col items-center space-y-4 py-4">
                   <SocialLink
-                    href="https://www.linkedin.com/in/tomadrien/"
+                    href=""
                     icon={<Linkedin className="h-4 w-4" />}
                     label="Linkedin"
                     glowColor="bg-blue-500/50"
                     bgGradient="bg-blue-500/20"
                   />
                   <SocialLink
-                    href="https://orsay.ai/"
+                    href=""
                     icon={<Globe className="h-4 w-4" />}
                     label="Website"
                     glowColor="bg-purple-500/50"
                     bgGradient="bg-purple-500/20"
                   />
                   <SocialLink
-                    href="https://x.com/noftomx"
+                    href=""
                     icon={<Twitter className="h-4 w-4" />}
                     label="Twitter"
                     glowColor="bg-gray-500/50"
